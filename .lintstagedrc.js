9fb8151564ecea3c16b888c7518bb1ee15b0025b
Mayuresh <mckakade@gmail.com>
2023-04-18 02:12:31 +0530

    husky setup with lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,11 @@
-const path = require('path')
+const path = require('path');
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
+    .join(' --file ')}`;
 
 module.exports = {
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-}
+};
