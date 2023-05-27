de0d3c4d9c9b7ae209f2877430f7696f06aa08a2
Mayuresh <mckakade@gmail.com>
2023-05-28 01:32:11 +0530

    fix: type declarations

--- typings.d.ts
+++ typings.d.ts
@@ -2,12 +2,3 @@ declare module '*.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
-
-declare global {
-  interface Window {
-    $: JQueryStatic;
-    jQuery: JQueryStatic;
-  }
-}
-
-export {};
