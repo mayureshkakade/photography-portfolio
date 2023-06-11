f983027b2e1f035a9251404000634352a09a41af
Mayuresh <mckakade@gmail.com>
2023-06-11 19:13:53 +0530

    feat: add carousel to home page

--- src/components/Home/Carousel/index.tsx
+++ src/components/Home/Carousel/index.tsx
@@ -1,5 +1,95 @@
-import { FC } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import { FC, useState } from 'react';
+
+import Image from 'next/image';
+import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
+import { getCarouselImages } from '../helper';
 
 export const Carousel: FC = () => {
-  return <></>;
+  const images = getCarouselImages();
+  const [{ currentSlide }, setState] = useState({
+    currentSlide: 0,
+    isLoading: true,
+  });
+
+  // Callback fired on next image request in carousel
+  const next = () => {
+    setState((state) => ({
+      ...state,
+      currentSlide: state.currentSlide + 1,
+    }));
+  };
+
+  // Callback fired on previous image request in carousel
+  const prev = () => {
+    setState((state) => ({
+      ...state,
+      currentSlide: state.currentSlide - 1,
+    }));
+  };
+
+  // Callback fired after the slide is changed in carousel
+  const updateCurrentSlide = (index: number) => {
+    if (currentSlide !== index) {
+      setState((state) => ({
+        ...state,
+        currentSlide: index,
+      }));
+    }
+  };
+
+  return (
+    <div
+      className="padding_top"
+      style={{
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      }}
+    >
+      <div onClick={prev} className="my-prev-arrow">
+        <i
+          className="fa fa-angle-left"
+          style={{ fontSize: '1.7em', color: '#8080807a' }}
+        />
+      </div>
+      <div onClick={next} className="my-next-arrow">
+        <i
+          className="fa fa-angle-right"
+          style={{ fontSize: '1.7em', color: '#8080807a' }}
+        />
+      </div>
+
+      <ResponsiveCarousel
+        showIndicators={false}
+        showStatus={false}
+        showThumbs={false}
+        infiniteLoop
+        autoPlay
+        stopOnHover={false}
+        interval={3000}
+        dynamicHeight
+        showArrows={false}
+        selectedItem={currentSlide}
+        onChange={updateCurrentSlide}
+      >
+        {images.map((item) => {
+          return (
+            <div key={item.id}>
+              <Image
+                alt="home page banner"
+                src={item.image}
+                width={1000}
+                height={1000}
+                style={{ width: '100%', height: 'auto' }}
+              />
+            </div>
+          );
+        })}
+      </ResponsiveCarousel>
+
+      {/* TODO: Add a skeleton loader component below */}
+      {/* {isLoading && <HomeCarouselSkeleton />} */}
+    </div>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,7 @@ import Home from '@/components/Home';
 export default function HomePage() {
   return (
     <>
-      {/* TODO: Remove below div and add component for home page*/}
       <Home />
-      <div style={{ height: '300vh' }}></div>
     </>
   );
 }
