0f8cd341f231e810c6323940cf2fbaccaf84c606
Mayuresh Kakade <44115669+mayureshkakade@users.noreply.github.com>
2023-06-11 16:33:18 +0530

    Setup album categories metadata (#44)
    * refactor: update pr template
    
    * feat: add metadata for albums
    
    * feat: update page tab title and description

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const pageTitle = `Omkar Kalgude Photography | Candid Wedding Photography and Cinematography | India | Pune | Maharashtra`;
+
+export const pageDescription = `Welcome to Omkar Kalgude Photography & Films. We are a team of
+      Candid Wedding Photographers and Cinematographers based in
+      Maharashtra, India and we are known for our fun, off-beat, and
+      modern approach for creating stories from the best day in your
+      life. Led by Omkar Kalgude, we started photographing weddings in
+      2018, and since then we have created hundreds of unique love
+      stories across the country. We are a passionate, creative, and
+      an incredibly motivated team. It will be our pleasure to be a
+      part of your beautiful journey.`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../../public/assets/css/style.css';
 
+import { pageDescription, pageTitle } from '@/constants';
+
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import Layout from '../../components/Layout';
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Script src="https://code.jquery.com/jquery-1.12.4.min.js"></Script>
       <Script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></Script>
       <Script src="assets/js/jquery.slicknav.min.js"></Script>
