32a451a81543806101a89acdcfab034d6bd03785
Mayuresh <mckakade@gmail.com>
2024-03-09 14:15:55 +0530

    feat: invoked components

--- src/pages/albums.tsx
+++ src/pages/albums.tsx
@@ -1,9 +1,14 @@
 import Carousel from '@/components/Carousel';
+import { Albums } from '@/components/Home/Albums';
+import { FeaturedAlbumsHeading } from '@/components/Home/Text';
+import { getAlbums } from '@/components/Home/helper';
 
-export default function Albums() {
+export default function GalleryAlbums() {
   return (
     <>
       <Carousel />
+      <FeaturedAlbumsHeading />
+      <Albums albums={getAlbums()} parentFeature="albums-page" />
     </>
   );
 }
