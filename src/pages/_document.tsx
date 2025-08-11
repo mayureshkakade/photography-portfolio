8d8363497dcf2b8c47639b7f7de18cfd15f7205d
Mayuresh Kakade <44115669+mayureshkakade@users.noreply.github.com>
2025-08-12 03:22:55 +0530

    feat: add vercel analytics (#64)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function Document() {
   return (
@@ -13,6 +14,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
