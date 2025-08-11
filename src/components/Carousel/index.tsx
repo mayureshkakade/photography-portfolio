d025cb490e22c9d75d8239acd2ba8e020a1afe23
mayureshkakade <mckakade@gmail.com>
2025-08-12 02:13:14 +0530

    feat: integrate gdrive images in albums carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,46 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import React, { useEffect, useState } from 'react';
-import { Carousel as ResponsiveCarousel} from 'react-responsive-carousel';
-import { getCarouselImages } from './helper';
+import React, { useState } from 'react';
+import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
+import { AppImageData } from '../types';
 
-export default function Carousel() {
+interface CarouselProps {
+  carouselImages: AppImageData[];
+}
+
+export default function Carousel({ carouselImages }: CarouselProps) {
   const [state, setState] = useState({
     currentSlide: 0,
     firstLoad: true,
-    isLoading: true,
   });
-
-  const carouselImages = getCarouselImages();
-
-  // Begin polling for the required lazy images tag and observe the images for intersection
-  useEffect(() => {
-    const lazyImagePoll = setInterval(() => {
-      if (
-        state.firstLoad &&
-        document.querySelectorAll('img.lazy_load_image').length > 0
-      ) {
-        clearInterval(lazyImagePoll);
-        const imageObserver = new IntersectionObserver((entry, imgObserver) => {
-          entry.forEach((element) => {
-            if (element.isIntersecting) {
-              const lazyImage = element.target as HTMLImageElement;
-              lazyImage.src = lazyImage.dataset.src ?? '';
-              lazyImage.classList.remove('lazy_load_image');
-              imgObserver.unobserve(lazyImage);
-            }
-          });
-        });
-
-        const arr = document.querySelectorAll('img.lazy_load_image');
-        arr.forEach((v) => {
-          imageObserver.observe(v);
-        });
-        setState((prev) => ({ ...prev, firstLoad: false }));
-      }
-    }, 200);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   // Callback fired on next image request in carousel
   const next = () => {
@@ -68,11 +40,6 @@ export default function Carousel() {
         currentSlide: index,
       }));
     }
-  };
-
-  // Callback fired when first image in carousel loads and carousel skeleton disappears
-  const stopLoader = () => {
-    setState((prev) => ({ ...prev, isLoading: false }));
   };
 
   return (
@@ -118,18 +85,12 @@ export default function Carousel() {
                 className="lazy_load_image"
                 style={{ height: '100%' }}
                 alt="Carousel"
-                onLoad={() => {
-                  if (item.id === 1) stopLoader();
-                }}
-                data-src={item.image}
-                src={item.thumbnail}
+                src={item.url}
               />
             </div>
           );
         })}
       </ResponsiveCarousel>
-      {/* TODO: Add Skeleton Component */}
-      {state.isLoading && 'Loading...'}
     </div>
   );
 }
--- src/pages/albums.tsx
+++ src/pages/albums.tsx
@@ -1,13 +1,17 @@
 import Carousel from '@/components/Carousel';
 import { Albums } from '@/components/Home/Albums';
 import { FeaturedAlbumsHeading } from '@/components/Home/Text';
-import { getAlbums } from '@/components/Home/helper';
-import { fetchAllAlbumsDetails } from '@/lib/google-drive-image';
-import { AlbumData } from '@/components/types';
+import { getAlbums, ALBUM_CAROUSEL_FOLDER_ID } from '@/components/Home/helper';
+import {
+  fetchAllAlbumsDetails,
+  fetchGoogleDriveImages,
+} from '@/lib/google-drive-image';
+import { AlbumData, AppImageData } from '@/components/types';
 import { GetServerSideProps } from 'next';
 
 interface GalleryAlbumsProps {
   albums: AlbumData[];
+  carouselImages: AppImageData[];
 }
 
 export const getServerSideProps: GetServerSideProps<
@@ -15,14 +19,22 @@ export const getServerSideProps: GetServerSideProps<
 > = async () => {
   try {
     const API_KEY = process.env.GOOGLE_API_KEY;
-
     let albums: AlbumData[] = [];
+    let carouselImages: AppImageData[] = [];
 
     if (API_KEY) {
       try {
         albums = await fetchAllAlbumsDetails(getAlbums(), API_KEY);
+        carouselImages = await fetchGoogleDriveImages(
+          ALBUM_CAROUSEL_FOLDER_ID,
+          API_KEY,
+          'large'
+        );
       } catch (error) {
-        console.error('Error fetching album details from Google Drive:', error);
+        console.error(
+          'Error fetching album details or carousel images from Google Drive:',
+          error
+        );
       }
     } else {
       console.warn('GOOGLE_API_KEY not found in environment variables');
@@ -31,6 +43,7 @@ export const getServerSideProps: GetServerSideProps<
     return {
       props: {
         albums,
+        carouselImages,
       },
     };
   } catch (error) {
@@ -38,15 +51,19 @@ export const getServerSideProps: GetServerSideProps<
     return {
       props: {
         albums: [],
+        carouselImages: [],
       },
     };
   }
 };
 
-export default function GalleryAlbums({ albums }: GalleryAlbumsProps) {
+export default function GalleryAlbums({
+  albums,
+  carouselImages,
+}: GalleryAlbumsProps) {
   return (
     <>
-      <Carousel />
+      <Carousel carouselImages={carouselImages} />
       <FeaturedAlbumsHeading />
       <Albums albums={albums} parentFeature="albums-page" />
     </>
